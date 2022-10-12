7d4af91c2b3cbcb56d8fed11747861de143466a8
micah <mc@micahcrenwelge.com>
2022-10-12 12:36:31 -0500

    my shit code

---
+++ BST.py
@@ -0,0 +1,24 @@
+
+# Need a user interface with 8 options
+# 1) Insert nodes
+# 2) Print tree
+# 3) Print number of leaves
+# 4) Print number of half nodes in T
+# 5) Print number of full nodes in T
+# 6) Print level the level order traversal of the tree
+# 7) Delete node with a specific value
+# 8) Exit program
+
+print("Enter a choice [1-8] from menu below:")
+print("1) Insert nodes")
+print("2) Print tree")
+print("3) Print number of leaves")
+print("4) Print number of half nodes in T")
+print("5) Print number of full nodes in T")
+print("6) Print level the level order traversal of the tree")
+print("7) Delete node with a specific value")
+print("8) Exit program")
+
+user_selection = int(input("Enter your choice: "))
+
+
